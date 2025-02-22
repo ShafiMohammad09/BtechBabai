d96e9715a17bfb8a1a60bef5c14d51938c481807
shafimohammad09 <shafimohammad5050@gmail.com>
2025-02-22 12:08:30 +0530

    resloveing the .env variables to load dynamically

--- public/script.js
+++ public/script.js
@@ -1,15 +1,17 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: "AIzaSyAIi0Qlg3-wbuaPXUQFPl2YtpeDqPRnT-Q",
-  apiKey: "import.meta.env.VITE_FIREBASE_API_KEY",
-  authDomain: "import.meta.env.VITE_FIREBASE_AUTH_DOMAIN",
-  databaseURL: "import.meta.env.VITE_FIREBASE_DATABASE_URL",
-  projectId: "import.meta.env.VITE_FIREBASE_PROJECT_ID",
-  storageBucket: "import.meta.env.VITE_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "import.meta.env.VITE_FIREBASE_APP_ID",
-  measurementId: "import.meta.env.VITE_FIREBASE_MEASUREMENT_ID"
+  apiKey: window.FIREBASE_API_KEY,
+  authDomain: window.FIREBASE_AUTH_DOMAIN,
+  databaseURL: window.FIREBASE_DATABASE_URL,
+  projectId: window.FIREBASE_PROJECT_ID,
+  storageBucket: window.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: window.FIREBASE_MESSAGING_SENDER_ID,
+  appId: window.FIREBASE_APP_ID,
+  measurementId: window.FIREBASE_MEASUREMENT_ID
 };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
